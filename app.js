ba6ecb9664e8baea5d9fba6d34c8c87cc65440de
Micky <69732917+Micky014@users.noreply.github.com>
2021-09-29 21:07:49 +0200

    change fx_version adamant to cerulean
    Thanks to del1an of the FiveM forum for reminding me that there are newer versions of fx_version.

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ RegisterCommand("manifest", function(src, args, rw) {
                     8: "resource_manifest_version \"05cfa83c-a124-4cfa-a768-c24a5811d8f9\""
                 }
         
-                let newMetaData = "fx_version 'adamant'\ngame 'gta5'\n";
+                let newMetaData = "fx_version 'cerulean'\ngame 'gta5'\n";
                 
                 for (j in oldMetaData) {
                     let metadata = oldMetaData[j]
